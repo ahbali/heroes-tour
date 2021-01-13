d9feb09a8ffd906cd51da7e28d8c12bfeddcf77b
mehdi <el.ahbali@gmail.com>
2021-01-13 10:28:08 +0100

    code formating wiht Prettier

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,26 +1,25 @@
 import { Component, OnInit } from '@angular/core';
 
 import { Hero } from '../hero';
-import { HEROES } from "../mock-heroes";
+import { HEROES } from '../mock-heroes';
 
 @Component({
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
-  styleUrls: ['./heroes.component.css']
+  styleUrls: ['./heroes.component.css'],
 })
 export class HeroesComponent implements OnInit {
   heroes = HEROES;
-  selectedHero?: Hero;//= { id: 0, name: "void" };
+  selectedHero?: Hero; //= { id: 0, name: "void" };
 
   hero: Hero = {
     id: 1,
-    name: 'Windstorm'
+    name: 'Windstorm',
   };
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
+  ngOnInit(): void {}
 
   onSelect(hero: Hero): void {
     this.selectedHero = hero;
