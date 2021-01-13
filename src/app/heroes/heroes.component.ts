298d9dd14395b9bc71fb3ed3b19fbe69a2062b61
mehdi <el.ahbali@gmail.com>
2021-01-13 13:09:13 +0100

    completed part 4

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 
 import { Hero } from '../hero';
+import { HeroService } from '../hero.service';
+import { MessageService } from '../message.service';
 import { HEROES } from '../mock-heroes';
 
 @Component({
@@ -9,19 +11,24 @@ import { HEROES } from '../mock-heroes';
   styleUrls: ['./heroes.component.css'],
 })
 export class HeroesComponent implements OnInit {
-  heroes = HEROES;
+  heroes: Hero[] = [];
   selectedHero?: Hero; //= { id: 0, name: "void" };
 
-  hero: Hero = {
-    id: 1,
-    name: 'Windstorm',
-  };
+  constructor(
+    private heroService: HeroService,
+    private messageService: MessageService
+  ) {}
 
-  constructor() {}
+  ngOnInit(): void {
+    this.getHeroes();
+  }
 
-  ngOnInit(): void {}
+  getHeroes(): void {
+    this.heroService.getHeroes().subscribe((heroes) => (this.heroes = heroes));
+  }
 
   onSelect(hero: Hero): void {
     this.selectedHero = hero;
+    this.messageService.add(`HeroesComponent: Selected hero id=${hero.id}`);
   }
 }
